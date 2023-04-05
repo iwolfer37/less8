b6f5c7b541e24e01215fd3c72b3837b1f2629e57
iWolfer <iwolfer@me.com>
2023-04-05 18:44:39 +0300

    Другій коміт в тест й папку зайву удалив

--- mane.py
+++ mane.py
@@ -7,10 +7,16 @@ def mane_function(data_dict, file_name):
             f.write(f"{key}: {value}\n")
         shoping_list = {'Помідорів': 4, 'Огірків': 3, 'Листя салату': 1}
 create_file(shoping_list, 'shoping_list.txt')
+#Це функція mane_function в яку я добавив свій код, який створює файл shoping_list.txt
 
-
-#Наша нова функція тест
+#Наша нова функція
 def test_function(file_name)
     data_dict = {}
-        with open(file_name, 'r') as f:
-            for line in f:
+    with open(file_name, 'r') as f:
+        for line in f:
+            #в цьому циклі ми забераємо пробіли й ділимо рядок
+            key, value = line.strip().split(': ')
+            data_dict[key] = int(value)
+    return data_dict
+shoping = test_function('shoping_list.txt')
+print(shoping)
