8b1f74a157c057efc8a13a8bac0677f9dd309479
iWolfer <iwolfer@me.com>
2023-04-05 16:48:12 +0300

    Добавив в код коментарі

--- mane.py
+++ mane.py
@@ -1,6 +1,9 @@
+#Створюємо наш словник та сам файл:
 def mane_function(data_dict, file_name):
+#відкриваємо файл в режимі запису:
     with open(file_name, 'w') as f:
         for key, value in data_dict.items():
+#записуємо словник й все в новому рядку:
             f.write(f"{key}: {value}\n")
         shoping_list = {'Помідорів': 4, 'Огірків': 3, 'Листя салату': 1}
 create_file(shoping_list, 'shoping_list.txt')
