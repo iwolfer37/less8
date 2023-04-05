0594e005d0cccfec73e2105012f5b1c01faaf30b
iWolfer <iwolfer@me.com>
2023-04-05 16:32:25 +0300

    Дві нові строчки коду для ношої функції які записуємо в словник

--- mane.py
+++ mane.py
@@ -1,3 +1,5 @@
 def mane_function(data_dict, file_name):
     with open(file_name, 'w') as f:
         for key, value in data_dict.items():
+            f.write(f"{key}: {value}\n")
+        shoping_list = {'Помідорів': 4, 'Огірків': 3, 'Листя салату': 1}
