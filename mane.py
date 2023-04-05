96489fa361f805316c768fb1ce09d6f5aacf26d8
iWolfer <iwolfer@me.com>
2023-04-05 17:19:50 +0300

    Добавиви test_function в гілці test

--- mane.py
+++ mane.py
@@ -7,4 +7,10 @@ def mane_function(data_dict, file_name):
             f.write(f"{key}: {value}\n")
         shoping_list = {'Помідорів': 4, 'Огірків': 3, 'Листя салату': 1}
 create_file(shoping_list, 'shoping_list.txt')
-#Це функція mane_function в яку я добавив свій код, який створює файл shoping_list.txt
+
+
+#Наша нова функція
+def test_function(file_name)
+    data_dict = {}
+        with open(file_name, 'r') as f:
+            for line in f:
