c032ad9a93bb7459747e0774ecaed83ac672cd20
iWolfer <iwolfer@me.com>
2023-04-05 16:37:27 +0300

    Фінальний етап сторення функції mane_function

--- mane.py
+++ mane.py
@@ -3,3 +3,5 @@ def mane_function(data_dict, file_name):
         for key, value in data_dict.items():
             f.write(f"{key}: {value}\n")
         shoping_list = {'Помідорів': 4, 'Огірків': 3, 'Листя салату': 1}
+create_file(shoping_list, 'shoping_list.txt')
+#Це функція mane_function в яку я добавив свій код, який створює файл shoping_list.txt
